7e1596e6ae5b97a00125a943dd7f2c38a15283f4
Sanaghufran <sanaghufran2005@gmail.com>
2024-08-02 22:18:29 +0530

    data coming from thunderclient

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -1,17 +1,18 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/User')
-router.post("/createuser",async(req,res)=>{
-    try{
-        await User.create({name:"sana",
-            password:"hbjbb76",
-            email:"sana@gmail.com",
-            location:"Pandara Road India Gate New Delhi"
-        })
-        res.json({success:true});
-    }catch(error){
-        console.log(error)
-        res.json({success:false});
-    }
-})
-module.exports = router
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+router.post("/createuser", async (req, res) => {
+  try {
+    await User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+      location: req.body.location
+    });
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false });
+  }
+});
+module.exports = router;
